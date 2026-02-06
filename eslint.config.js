bb84d4f2761c084fcf8596c37ff32ff11045c6e6
Ross A. Reyman <rareyman@gmail.com>
2026-02-05 16:32:45 -0800

    feat: update ESLint configuration and add html plugin; remove index.html

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,29 @@
-import js from "@eslint/js";
-import prettier from "eslint-config-prettier";
-import globals from "globals";
+import js from '@eslint/js'
+import prettier from 'eslint-config-prettier'
+import globals from 'globals'
+import html from 'eslint-plugin-html'
 
 export default [
-    js.configs.recommended,
-    prettier,
-    {
-        languageOptions: {
-            ecmaVersion: 2022,
-            sourceType: "module",
-            globals: {
-                ...globals.browser,
-                ...globals.node,
-                Alpine: "readonly" // Tell ESLint Alpine is a global variable
-            }
-        },
-        rules: {
-            // Add any custom rules from your old config here
-            "no-unused-vars": "warn",
-            "no-console": "off"
-        }
-    }
-];
+	{
+		files: ['**/*.html'],
+		plugins: { html },
+	},
+	js.configs.recommended,
+	prettier,
+	{
+		languageOptions: {
+			ecmaVersion: 2022,
+			sourceType: 'module',
+			globals: {
+				...globals.browser,
+				...globals.node,
+				Alpine: 'readonly', // Tell ESLint Alpine is a global variable
+			},
+		},
+		rules: {
+			// Add any custom rules from your old config here
+			'no-unused-vars': 'warn',
+			'no-console': 'off',
+		},
+	},
+]
